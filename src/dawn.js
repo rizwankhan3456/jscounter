d77edf1767b0e9cc0415a5b23613e60ae340739d
Rizwankhane <rizwan@cedcoss.com>
2022-02-17 10:55:02 +0530

    counter

--- src/dawn.js
+++ src/dawn.js
@@ -1,12 +1,17 @@
 var data = 0;
 
-document.getElementById("counting").innerText = data;
+document.getElementById("counting").value = data;
 
 function increment() {
   data = data + 1;
-  document.getElementById("counting").innerText = data;
+  document.getElementById("counting").value = data;
 }
 function decrement() {
   data = data - 1;
-  document.getElementById("counting").innerText = data;
+  document.getElementById("counting").value = data;
 }
+
+function display(){
+  document.getElementById("show").innerHTML= data;
+}
+
